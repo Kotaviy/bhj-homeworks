09fb2f2e1f991259118fac37be36e0249b751827
Kseniya Radiseva <Kotaviy@gmail.com>
2022-02-10 22:40:48 +0500

    Исправленное задание

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,18 +1,19 @@
 const modalMain = document.getElementById('modal_main');
-const closeElements = document.getElementsByClassName('modal__close');
-const successBtn = document.getElementsByClassName('show-success')
-const modalSuccess = document.getElementById('modal_success')
+const closeElements = document.querySelectorAll('.modal__close');
+const closeElementsArr = Array.from(closeElements);
+const successBtn = document.getElementsByClassName('show-success');
+const modalSuccess = document.getElementById('modal_success');
 
 modalMain.className = 'modal modal_active';
 
-closeElements[0].closest('.modal_active').onclick = function() {
-    modalMain.className = 'modal'
-}
+closeElementsArr.forEach((elem) => {
+    elem.onclick = function() {
+        this.closest('.modal').className = 'modal';
+        console.log(this.closest('.modal'))
+    }
+})
 
 successBtn[0].onclick = function() {
-    modalSuccess.className = 'modal modal_active'
-}
-
-closeElements[2].onclick = function() {
-    modalSuccess.className = 'modal'
+    modalSuccess.className = 'modal modal_active';
+    modalMain.className = 'modal';
 }
