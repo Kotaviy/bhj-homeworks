abf890862dde6de3d6f506a9ea4a28c0e50cb801
Kseniya Radiseva <Kotaviy@gmail.com>
2022-02-16 20:38:58 +0500

    Исправленное задание

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,33 +2,30 @@ const nextBtn = document.querySelector('.slider__arrow_next');
 const prevBtn = document.querySelector('.slider__arrow_prev');
 const sliders = document.querySelectorAll('.slider__item');
 const slidersArr = Array.from(sliders);
-let activeSlideIndex = 0;
 
-
-let getNextIndex = function (index) {
-    if(index < slidersArr.length-1) {
-        return activeSlideIndex + 1;
-    } else {
-        return 0;
-    }
+function getIndex(element) {
+    if(element.className === 'slider__item slider__item_active')
+    return true
 }
 
-let getPrevIndex = function (index) {
-    if(index > 0) {
-        activeSlideIndex -= 1;
-    } else {
-        activeSlideIndex = slidersArr.length-1;
-    }
+let activeIndex = slidersArr.findIndex(getIndex);
+
+const getNextIndex = function (index) {
+    return index < slidersArr.length -1 ? activeIndex + 1 : 0;
+}
+
+const getPrevIndex = function (index) {
+    return index > 0 ? activeIndex -1 : slidersArr.length-1;
 }
 
 nextBtn.onclick = function() {
-    slidersArr[activeSlideIndex].className = 'slider__item';
-    activeSlideIndex = getNextIndex(activeSlideIndex);
-    slidersArr[activeSlideIndex].className = 'slider__item slider__item_active';
+    slidersArr[activeIndex].className = 'slider__item';
+    activeIndex = getNextIndex(activeIndex);
+    slidersArr[activeIndex].className = 'slider__item slider__item_active';
 }
 
 prevBtn.onclick = function() {
-    slidersArr[activeSlideIndex].className = 'slider__item';
-    getPrevIndex(activeSlideIndex);
-    slidersArr[activeSlideIndex].className = 'slider__item slider__item_active'
+    slidersArr[activeIndex].className = 'slider__item';
+    activeIndex = getPrevIndex(activeIndex);
+    slidersArr[activeIndex].className = 'slider__item slider__item_active'
 }
