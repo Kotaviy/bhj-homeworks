366727b54a07cf7884717c5c59e8ca938f40755a
Kseniya Radiseva <Kotaviy@gmail.com>
2022-03-20 17:01:58 +0500

    Готовое задание

---
+++ html-forms/chat/task.js
@@ -0,0 +1,38 @@
+const chatWidget = document.querySelector('.chat-widget');
+const input = document.getElementById('chat-widget__input');
+const messages = document.getElementById('chat-widget__messages');
+let time = new Date().toLocaleTimeString().slice(0,-3);
+
+const responsesArr = [
+    'Добрый день',
+    'Кто здесь?',
+    'Вас плохо слышно',
+    'Перезвоните позже, какие-то проблемы со связью',
+    'До свидания',
+]
+
+function botResponse() {
+    let random = Math.floor(Math.random() * responsesArr.length);
+    return (responsesArr[random]);
+}
+
+chatWidget.addEventListener('click', () => {
+    chatWidget.classList.add('chat-widget_active');
+})
+
+input.addEventListener('keyup', () => {
+    if(event.key === 'Enter') {
+        messages.innerHTML += `
+        <div class="message message_client">
+            <div class="message__time">${time}</div>
+                <div class="message__text">${input.value}</div>
+                    </div>
+                    `
+        messages.innerHTML += `
+        <div class="message">
+            <div class="message__time">22:10</div>
+                <div class="message__text">${botResponse()}</div>
+                    </div>  
+                    `
+    }
+})
